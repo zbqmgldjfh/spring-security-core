d6f63bb94e64a4e92c2d6a98228f2173964d5e97
jiwoo <zbqmgldjfh@gmail.com>
2022-10-26 20:18:05 +0900

    Initial commit

---
+++ src/test/java/shine/me/springsecuritycore/SpringSecurityCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package shine.me.springsecuritycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
