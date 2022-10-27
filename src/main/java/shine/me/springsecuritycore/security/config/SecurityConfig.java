2eaf794db7222cd115bbb6f946cb9ef0a9e131f8
jiwoo <zbqmgldjfh@gmail.com>
2022-10-27 11:38:26 +0900

    feat(SecurityConfig): 정적 파일 관리를 위한 WebIgnore 설정

--- src/main/java/shine/me/springsecuritycore/security/config/SecurityConfig.java
+++ src/main/java/shine/me/springsecuritycore/security/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package shine.me.springsecuritycore.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -38,5 +40,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
         ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
